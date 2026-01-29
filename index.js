daa277fa32c72fa49a862a802e18878b641197c0
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-01-29 18:08:42 +0700

    Added EJS Template

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ejs from "ejs";
 
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs")
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
