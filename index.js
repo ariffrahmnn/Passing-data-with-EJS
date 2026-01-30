a163c0903f61040a102d3816d950c741e78d50b1
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-01-30 11:27:24 +0700

    Added some details for readable matters

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
+//Importing reqired packages
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 
+//Initiating the express app
 const app = express();
-const port = 3000;
+const port = 3000; // Currently port
 
+//Set up EJS 
 app.set("view engine", "ejs")
 
+//Simple Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//GET 
 app.get("/", (req, res) => {
+  
+  //initiate variable
   const data = {
     title: "Input your name here!!"
   }
   res.render("index.ejs", data);
 });
 
+//POST
 app.post("/submit", (req, res) => {
 
+  //Initiate varaibles
   let wordOfName = req.body["fName"] + req.body["lName"];
   let fullname = wordOfName.length;
 
   res.render("index.ejs", {
-    finalNumbers: fullname
+    dynamicNumber: fullname
   });
+
 });
 
+//Listen 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
